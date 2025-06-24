251d865beffe35711cf5a18225a9ba713e52003b
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-06-24 21:22:49 +0800

    feat: update sqlc queries and schemas and config

---
+++ sql/schemas/00001_users.sql
@@ -0,0 +1,34 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS moddatetime;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TYPE user_type AS ENUM ('seeker', 'poster');
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  user_type user_type NOT NULL DEFAULT 'seeker',
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  version INT DEFAULT 1
+);
+
+CREATE INDEX idx_users_email ON users (email);
+
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION moddatetime (updated_at);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP EXTENSION IF EXISTS "moddatetime"
+DROP EXTENSION IF EXISTS "uuid-ossp";
+DROP TYPE IF EXISTS user_type;
+DROP TABLE IF EXISTS users;
+DROP TRIGGER update_users_updated_at ON users;
+-- +goose StatementEnd
